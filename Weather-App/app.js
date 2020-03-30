d391bfe48c34a9cfa8b2cfd4cd5f908142c83a10
Rishiv15 <rishivchhabria@gmail.com>
2020-03-30 15:32:30 +0530

    Added current weather to day_weather

--- Weather-App/app.js
+++ Weather-App/app.js
@@ -8,15 +8,11 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-console.log("hi")
-
 app.get("/", function(req, res){
-    request('http://api.openweathermap.org/data/2.5/weather?q=mumbai&appid=48164502b664fb28643399e9f69d1016', function(error,response,body){
-        if(!error && response.statusCode==200){
-            console.log(body);
-        }
-        else{console.log("Error occured")}
-    })
+    res.render("home");
+});
+
+app.get("/home", function(req, res){
     res.render("home");
 });
     
@@ -41,15 +37,112 @@ app.get("/country_map", function(req, res){
     res.render("country_map");
 });
 
-app.get("/day_weather", function(req, res){
-    res.render("day_weather");
-});
+var place = 'mumbai'; 
 
 app.get("/index", function(req, res){
-    /*var place = req.body.place;
-    console.log(req.body);
-    console.log(place);*/
-    res.render("index");
+    if(req.query.place){
+        place = req.query.place;
+    }
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + place + '&appid=48164502b664fb28643399e9f69d1016&units=metric';
+    request(url, function(error,response,body){
+        
+        if(!error && response.statusCode == 200){
+            var data = JSON.parse(body);
+            var temp = data["main"]["temp"];
+            var coord = data["coord"];
+            var icon = data["weather"][0]["icon"];
+            var pressure = data.main.pressure;
+            var humidity = data.main.humidity;
+            var wind_speed = data.wind.speed;
+            var sunrise = data.sys.sunrise + data.timezone;
+
+            date = new Date(sunrise * 1000); 
+            utcString = date.toUTCString(); 
+            sunrise = utcString.slice(-11, -4);
+            
+            date = String(date);
+            date = date.slice(4, 10);
+
+            obj = {
+                place: place,
+                icon: icon,
+                coord: coord,
+                temp: temp,
+                pressure: pressure,
+                humidity: humidity,
+                wind_speed: wind_speed,
+                date: date,
+
+            };
+            console.log(obj);
+            res.render("index", obj);
+        }
+        else{
+            console.log("Error");
+        }
+    });
+});
+
+app.get("/day_weather", function(req, res){
+
+    if(req.query.place){
+        place = req.query.place;
+    }
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + place + '&appid=48164502b664fb28643399e9f69d1016&units=metric';
+
+    request(url, function(error, response, body){
+        if(!error && response.statusCode == 200){
+            var data = JSON.parse(body);
+
+            var pressure = data.main.pressure;
+            var humidity = data.main.humidity;
+            var wind_speed = data.wind.speed;
+            var temp = data["main"]["temp"];
+            var coord = data["coord"];
+            var icon = data["weather"][0]["icon"];
+            var cond = data["weather"][0]["main"];
+            var temp_feels = data.main.feels_like;
+            var temp_min = data.main.temp_min;
+            var temp_max = data.main.temp_max;
+            var clouds = data.clouds.all;
+            var sunrise = data.sys.sunrise + data.timezone;
+            var sunset = data.sys.sunset + data.timezone;
+            var wind_deg = data.wind.deg;
+
+            date = new Date(sunrise * 1000); 
+            utcString = date.toUTCString(); 
+            sunrise = utcString.slice(-11, -4);
+            
+            dateObj = new Date(sunset * 1000); 
+            utcString = dateObj.toUTCString(); 
+            sunset = utcString.slice(-11, -4);
+            date = String(date);
+            date = date.slice(0, 15);
+
+            var obj = {
+                place: place,
+                icon: icon,
+                cond: cond,
+                temp: temp,
+                temp_max: temp_max,
+                temp_min: temp_min,
+                temp_feels: temp_feels,
+                pressure: pressure,
+                humidity: humidity,
+                wind_speed: wind_speed,
+                clouds: clouds,
+                sunrise: sunrise,
+                sunset: sunset,
+                wind_deg: wind_deg,
+                coord: coord,
+                date: date
+            };
+            res.render("day_weather", obj);
+        }
+        else{
+            console.log("Error");
+        }
+    }); 
 });
 
 app.get("/pressure_map", function(req, res){
