146a275356be6a8e2f2f40d4ca29c7dc835946b1
yashwik <yashwikpanchal@gmail.com>
2020-04-21 23:13:18 +0530

    Authentication

--- Weather-App/app.js
+++ Weather-App/app.js
@@ -4,7 +4,9 @@ var bodyParser  =   require("body-parser");
 var request     =   require("request");
 var mongo       =   require("mongodb");
 var mongoose    =   require("mongoose");
-
+var passport    =   require("passport");
+var LocalStrategy = require("passport-local");
+var User        =   require("./models/user");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -17,6 +19,20 @@ app.set("view engine", "ejs");
 
 mongoose.connect("mongodb://localhost:27017/weather", {useNewUrlParser: true, useUnifiedTopology: true});
 
+app.use(require("express-session")({
+    secret : "weather app",
+    resave : false,
+    saveUninitialized : false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
 var weatSchema = new mongoose.Schema({
     temp: Number,
     humidity: Number
@@ -162,6 +178,7 @@ app.get("/comparison",function(req,res){
     else{
         console.log('Api1 failed');
     }
+
     
    });
   
@@ -400,7 +417,6 @@ var chart_temp = [], chart_hum = [];
 var days_chart = [];
 
 app.get("/index", function(req, res){
-
     if(req.query.place)
         place = req.query.place;
     
@@ -764,7 +780,50 @@ app.get("/country_map", function(req, res){
     res.render("country_map");
 });
 
-module.exports = chart_temp; 
+module.exports = chart_temp;
+
+//AUTH ROUTES
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username, email: req.body.email});
+    User.register(newUser, req.body.password, function(err,user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/index");
+        });
+    });
+});
+
+app.get("/login", function(req, res){
+    res.render("login");
+})
+
+app.post("/login", passport.authenticate("local",
+    {
+        successRedirect: "/index",
+        failureRedirect: "/login"
+    }), function(req, res){
+})
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/index")
+})
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+//AUTH ROUTES END
 
 app.get("*", function(req, res){
     res.send("Page not found");
